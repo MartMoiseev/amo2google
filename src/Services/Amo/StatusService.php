b06ff695abec756e7e6ce3e3caccf9d17fd5112a
MartMoiseev <moiseev@mart.ru>
2020-04-14 23:51:01 +0300

    FIX array statuses

--- src/Services/Amo/StatusService.php
+++ src/Services/Amo/StatusService.php
@@ -20,9 +20,9 @@ class StatusService
     public function __construct(ParameterBagInterface $parameterBag)
     {
         $this->statuses = [
-            AmoStatus::STATUS_NEW_LEAD => $parameterBag->get('amo.status.new_lead'),
-            AmoStatus::STATUS_TEST_PERIOD => $parameterBag->get('amo.status.test_period'),
-            AmoStatus::STATUS_PAID_ONE_MONTH => $parameterBag->get('amo.status.paid_one_month'),
+            AmoStatus::STATUS_NEW_LEAD => $this->toArray($parameterBag->get('amo.status.new_lead')),
+            AmoStatus::STATUS_TEST_PERIOD => $this->toArray($parameterBag->get('amo.status.test_period')),
+            AmoStatus::STATUS_PAID_ONE_MONTH => $this->toArray($parameterBag->get('amo.status.paid_one_month')),
         ];
     }
 
@@ -32,9 +32,21 @@ class StatusService
      */
     public function create(int $statusId): AmoStatus
     {
-        $key = array_search($statusId, $this->statuses);
-        $status = $key ? $key : '';
+        foreach ($this->statuses as $status => $ids) {
+            $key = in_array($statusId, $ids);
+            if ($key) {
+                return new AmoStatus($statusId, $status);
+            }
+        }
 
-        return new AmoStatus($statusId, $status);
+        return new AmoStatus($statusId, '');
+    }
+
+    private function toArray(string $ids): array
+    {
+        // очищаем от лишних пробелов
+        str_replace(' ', '', $ids);
+        // формируем массив
+        return explode(',', $ids);
     }
 }
